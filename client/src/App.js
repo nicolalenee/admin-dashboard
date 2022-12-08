148044fe132b5bec51c35ea911567773edc61709
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-12-07 20:32:46 -0500

    create Team page data grid

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar'
 import Dashboard from './scenes/dashboard';
 import { Routes, Route } from 'react-router-dom';
-// import Team from './scenes/Team';
+import Team from './scenes/team';
 // import Invoices from './scenes/invoices';
 // import Contacts from './scenes/Contacts';
 // import Bar from './scenes/bar';
@@ -29,7 +29,7 @@ function App() {
           <Topbar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            {/* <Route path="/team" element={<Team />} /> */}
+            <Route path="/team" element={<Team />} />
             {/* <Route path="/contacts" element={<Contacts />} /> */}
             {/* <Route path="/invoices" element={<Invoices />} /> */}
             {/* <Route path="/form" element={<Form />} /> */}
